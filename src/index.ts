4d7e1db3d7986b402d6b37c39862b4834882f5c3
fadiinho <ffadiinho@gmail.com>
2022-08-18 01:27:45 -0300

    fix: cors error

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,11 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(function(_req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.get('/', (_req, res) => {
   res.json({ ok: true, message: 'Api is working!'});
