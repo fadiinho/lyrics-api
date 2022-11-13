2f699e6a6bd77ebe94d2f197ba63e2e5ea76311c
fadiinho <ffadiinho@gmail.com>
2022-11-13 15:14:14 -0300

    fix: add cors

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from "cors";
 
 import { searchRoutes } from './routes';
 
@@ -6,6 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(cors());
 
 app.get('/', (_req, res) => {
   res.json({ ok: true, message: 'Api is working!'});
